465a77a11d464c28bae88cf98faf6df065c8d7a0
007steve <stephen.plummer1.sp@gmail.com>
2020-12-30 14:31:22 -0500

    updated firebase

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,14 @@
 import firebase from "firebase";
 const  firebaseConfig = {
-    apiKey: "AIzaSyC5hBD8PxeIBrijQbUD_tbhJUjbOf3_Gw4",
-    authDomain: "twitter-clone-caf6f.firebaseapp.com",
-    databaseURL: "https://twitter-clone-caf6f.firebaseio.com",
-    projectId: "twitter-clone-caf6f",
-    storageBucket: "twitter-clone-caf6f.appspot.com",
-    messagingSenderId: "776817939107",
-    appId: "1:776817939107:web:dec66158973758b7b8a516",
-    measurementId: "G-VBV3C30YB1"
-  };
+  apiKey: "AIzaSyD6N6FbBYV-gSYx_ZTiuyaAQw7r0Q5aXMk",
+  authDomain: "twitter-ff70b.firebaseapp.com",
+  databaseURL: "https://twitter-ff70b.firebaseio.com",
+  projectId: "twitter-ff70b",
+  storageBucket: "twitter-ff70b.appspot.com",
+  messagingSenderId: "489303156897",
+  appId: "1:489303156897:web:d1f2a26a3c9081c10128e0",
+  measurementId: "G-YB18LZLNN9"
+};
 
   const firebaseApp = firebase.initializeApp(firebaseConfig);
   
